b6a34dacf7f3a2789be4750267c4915be7a2cfae
GTBAE-dev <gyutak93@gmail.com>
2021-06-07 20:49:40 +0900

    Step 2. Start Screen  - drawing start button(function)  - screen update

--- MemoryGame.py
+++ MemoryGame.py
@@ -1,5 +1,8 @@
 import pygame
 
+def display_start_button(): # start button 화면 표시 함수
+    pygame.draw.circle(screen, WHITE, start_button.center, 60, 5) #(Surface, Color, Pos, Radius, Width) 의 circle
+
 ''' 1. 기본 설정 '''
 pygame.init() # 초기화
 ''' 1-1. 화면'''
@@ -12,7 +15,11 @@ pygame.display.set_caption("MEMORY GAME")
 ''' 1-4. 음악 '''
 
 ''' 2. 게임 내 요소 '''
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
 ''' 2-1. 배경 '''
+start_button = pygame.Rect(0, 0, 120, 120) # (left, top, width, height) 의 Rect
+start_button.center = (120, screen_height - 120) # start_button Rect 중심 이동 (0, 0) → (120, screen_height - 120)
 ''' 2-2. 캐릭터 '''
 ''' 2-3. 무기 '''
 ''' 2-4. 적 '''
@@ -25,9 +32,16 @@ while running: # running == True
     for event in pygame.event.get(): # 이벤트 종류에 따른 동작
         if event.type == pygame.QUIT: # 창 닫힘 버튼
             running = False
+    
+
     ''' 3-3. 게임 내 요소 위치 정의 '''
     ''' 3-4. 충돌처리 '''
     ''' 3-5. 화면에 그리기 '''
+    screen.fill(BLACK) # 화면 채우기(이후 그리는 것들 겹치지 않기 위함)
+    
+    display_start_button() # 시작 버튼 표시
+
+    pygame.display.update() # 화면 업데이트
 
 ''' 4. 종료 '''
 pygame.quit()
